760d36917128e543c35ef7df017dcb133d03e634
KaterynaNat <kate.naturkach@gmail.com>
2025-04-04 14:43:09 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,32 @@
 import * as authService from '../services/auth.js';
 
+export const register = async (req, res) => {
+  const user = await authService.register(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data: user,
+  });
+};
+
+export const login = async (req, res) => {
+  const { accessToken, refreshToken } = await authService.login(req.body);
+
+  res
+    .cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    })
+    .status(200)
+    .json({
+      status: 200,
+      message: 'Successfully logged in an user!',
+      data: { accessToken },
+    });
+};
+
 export const refresh = async (req, res) => {
   const { refreshToken } = req.cookies;
 
